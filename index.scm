983e7abcc537fa683be15ade495794f9fc206be0
Hisai Toru <toru@torus.jp>
2014-06-27 16:41:38 +0900

    added index page

--- index.scm
+++ index.scm
@@ -25,3 +25,24 @@
 (define-http-handler "/exit"
   (^[req app]
     (sys-exit 0)))
+
+(define-http-handler "/"
+  (^[req app]
+    (let ([width (x->number (or (request-param-ref req "width") 640))]
+          [height (x->number (or (request-param-ref req "height") 480))]
+          [rows (x->number (or (request-param-ref req "rows") 1))]
+          [cols (x->number (or (request-param-ref req "cols") 1))])
+      (define (img-src row col)
+        #`"/render.jpg?size=,|width| ,|height|&frame=,(* col (/ width cols)) ,(* row (/ height rows)) ,(/ width cols) ,(/ height rows)"
+        )
+      (respond/ok
+       req
+       `(sxml (table
+               ,@(map (^[row]
+                        `(tr ,@(map (^[col]
+                                      `(td (img (@ (src ,(img-src row col)))))
+                                      ) (iota cols)))
+                        ) (iota rows))
+               )))
+
+    )))
