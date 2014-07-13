26b3b0ef9967876957fba341af061068c2a97371
Hisai Toru <toru@torus.jp>
2014-07-13 23:41:57 +0900

    added render.ppm

--- index.scm
+++ index.scm
@@ -19,12 +19,38 @@
             [outfile #`"out,(random-integer 1000000).jpg"])
         (sys-system
          #`"gosh raytracer.scm ,opt-size ,opt-frame | ppmtojpeg -quality 100 > ,outfile")
-        (respond/ok req `(file ,outfile) :content-type "image/jpg")
+        (let1 result (respond/ok req `(file ,outfile) :content-type "image/jpg")
+          (sys-unlink outfile)
+          result)
       ))))
+
+(define raytracer-mod (make-module #f))
+(load "./raytracer.scm" :environment raytracer-mod)
+
+(define-http-handler "/render.ppm"
+  (^[req app]
+    (random-source-randomize! default-random-source)
+    (let ([param-size (request-param-ref req "size")]
+          [param-frame (request-param-ref req "frame")])
+      (let ([opt-size (if param-size #`"-s ,param-size" "")]
+            [opt-frame (if param-frame #`"-f ,param-frame" "")]
+            [outfile #`"out,(random-integer 1000000).jpg"])
+        (respond/ok req
+                    (with-output-to-string
+                      (lambda ()
+                        ((global-variable-ref raytracer-mod 'main)
+                         `(self ,@(string-split opt-size #/\s+/)
+                                ,@(string-split opt-frame #/\s+/)))))
+                    :content-type "image/x-portable-pixmap")))))
 
 (define-http-handler "/exit"
   (^[req app]
     (sys-exit 0)))
+
+(define-http-handler "/sys"
+  (^[req app]
+    (sys-system "ls")
+    (respond/ok req "ok")))
 
 (define-http-handler "/"
   (^[req app]
