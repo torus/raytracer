5e339c8df3f92218c5bd4513a32424155c37e63b
Hisai Toru <toru@torus.jp>
2014-08-21 01:17:29 +0900

    call/cc works

--- worker.scm
+++ worker.scm
@@ -13,51 +13,83 @@
 
 ; REDISCLOUD_URL=redis://rediscloud:lmzBksmjjRkVf2FI@pub-redis-10787.us-east-1-2.1.ec2.garantiadata.com:10787
 
+(define *passwd* #f)
+
 (define (connect-redis)
   (rxmatch-let (rxmatch #/(?:\w+):\/\/(?:\w+):(\w+)@([^:]+):(\d+)/
                         (sys-getenv "REDISCLOUD_URL"))
     (value passwd host port)
 ;    #?=passwd #?=host #?=port
-    (let1 redis (redis-open-async host port)
-      ((redis-async-auth redis passwd) (lambda (res) (print #`"auth response: ,res")))
-      redis)))
+    (set! *passwd* passwd)
+    (redis-open-async host port)))
 
 (define (gen-update-proc)
-  (define (wait-for-response proc yield next)
-    (call/cc (lambda (cont)
-               (proc (lambda (res)
-                       (cont res)))
-               (yield (lambda (pub sub) (next))))))
-
   (lambda (pub sub)
+;    (print 100)
     (call/cc
      (lambda (yield)
-       (call/cc
-        (lambda (next)
-          (print (wait-for-response (redis-async-set pub "xxx" 123) yield next)
-                 )))
-       (print "yyyyyyy")
-       (call/cc
-        (lambda (next)
-          (print (wait-for-response (redis-async-get pub "xxx") yield next)
-                 )))
+       (define (wait-for-response proc)
+;         (print 102)
+         (call/cc (lambda (next)
+;                    (print 103)
+                    (call/cc (lambda (cont)
+;                               (print 104)
+                               (proc (lambda (res)
+;                                       (print 105 res)
+                                       (cont res)))
+;                               (print 106)
+                               (yield (lambda (pub sub) (next))))))))
+
+       (worker-main wait-for-response pub sub)
+;       (print 107)
        ))))
 
-(define update!
+(define (worker-main yield pub sub)
+  (print 0)
+  (print "auth pub " (yield (redis-async-auth pub *passwd*)))
+  (print 1)
+  (print "auth sub " (yield (redis-async-auth sub *passwd*)))
+  (print 2)
+
+  (print (yield (redis-async-set pub "xxx" 123)))
+  (print "yyyyyyy")
+  (print (yield (redis-async-get pub "xxx")))
+
+  (print 3)
+
+  ;; #?=(yield (redis-async-subscribe sub "render-task"))
+
+  ;; (let ((waiting #t))
+  ;;   (redis-async-set-subscribe-handler!
+  ;;    sub (lambda (res) #?=res (set! waiting #f)))
+
+  ;;   (while waiting (yield values))
+
+  ;;   (print (yield (redis-async-set pub "xxx" 123)))
+  ;;   (print "yyyyyyy")
+  ;;   (print (yield (redis-async-get pub "xxx")))
+  ;;   )
+
+  (sys-exit 0)
+  )
+
+#;(define update!
   (let ((proc (gen-update-proc)))
     (lambda (pub sub)
+      (print proc)
       (set! proc (proc pub sub)))))
 
 (define (main args)
   (let ((pub (connect-redis))
         (sub (connect-redis)))
 
+    ((gen-update-proc) pub sub)
+
     (while #t
-      (queue-length (ref pub 'hndl-queue))
-      (until (queue-empty? (ref pub 'hndl-queue)) (redis-async-update! pub))
-      (until (queue-empty? (ref sub 'hndl-queue)) (redis-async-update! sub))
-      ;; (redis-async-update! pub)
-      ;; (redis-async-update! sub)
-      (update! pub sub)
-      (sys-nanosleep (expt 10 6)))      ; sleep 1ms
+      ;; (until (queue-empty? (ref pub 'hndl-queue)) (redis-async-update! pub))
+      ;; (until (queue-empty? (ref sub 'hndl-queue)) (redis-async-update! sub))
+      (redis-async-update! pub)
+      (redis-async-update! sub)
+      ;(update! pub sub)
+      (sys-nanosleep (expt 10 8)))      ; sleep 100ms
   ))
