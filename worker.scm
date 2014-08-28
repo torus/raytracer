36f065abb18f04f14fb5479fae1e0623ffc2b51b
Hisai Toru <toru@torus.jp>
2014-08-29 00:13:35 +0900

    worker wip...

--- worker.scm
+++ worker.scm
@@ -1,6 +1,8 @@
 ;;; -*- mode: scheme; coding: utf-8 -*-
 
+(use srfi-11)
 (use gauche.threads)
+(use util.match)
 (use util.queue)
 
 (use redis)
@@ -12,72 +14,64 @@
 (define spheres ((global-variable-ref raytracer-mod 'make-scene)))
 
 ; REDISCLOUD_URL=redis://rediscloud:lmzBksmjjRkVf2FI@pub-redis-10787.us-east-1-2.1.ec2.garantiadata.com:10787
-
+; REDISCLOUD_URL=redis://rediscloud@localhost:6379
 (define *passwd* #f)
 
 (define (connect-redis)
-  (rxmatch-let (rxmatch #/(?:\w+):\/\/(?:\w+):(\w+)@([^:]+):(\d+)/
+  (rxmatch-let (rxmatch #/(?:\w+):\/\/(?:\w+)(?::(\w+))?@([^:]+):(\d+)/
                         (sys-getenv "REDISCLOUD_URL"))
-    (value passwd host port)
-;    #?=passwd #?=host #?=port
-    (set! *passwd* passwd)
-    (redis-open-async host port)))
+      (value passwd host port)
+    (set! *passwd* #?=passwd)
+    (redis-open-async #?=host #?=port)))
 
 (define (gen-update-proc)
   (lambda (pub sub)
-;    (print 100)
     (call/cc
      (lambda (yield)
        (define (wait-for-response proc)
-;         (print 102)
          (call/cc (lambda (next)
-;                    (print 103)
                     (call/cc (lambda (cont)
-;                               (print 104)
                                (proc (lambda (res)
-;                                       (print 105 res)
                                        (cont res)))
-;                               (print 106)
                                (yield (lambda (pub sub) (next))))))))
 
        (worker-main wait-for-response pub sub)
-;       (print 107)
        ))))
 
 (define (worker-main yield pub sub)
-  (print 0)
-  (print "auth pub " (yield (redis-async-auth pub *passwd*)))
-  (print 1)
-  (print "auth sub " (yield (redis-async-auth sub *passwd*)))
-  (print 2)
+  (define task-queued #f)
 
-  (print (yield (redis-async-set pub "xxx" 123)))
-  (print "yyyyyyy")
-  (print (yield (redis-async-get pub "xxx")))
+  (when *passwd*
+    #?=(yield (redis-async-auth pub *passwd*))
+    #?=(yield (redis-async-auth sub *passwd*)))
 
-  (print 3)
+  #?=(yield (redis-async-set sub "a" 123))
+  #?=(yield (redis-async-get sub "a"))
 
-  ;; #?=(yield (redis-async-subscribe sub "render-task"))
+  #?=(yield (redis-async-subscribe sub "task"))
 
-  ;; (let ((waiting #t))
-  ;;   (redis-async-set-subscribe-handler!
-  ;;    sub (lambda (res) #?=res (set! waiting #f)))
+  (while #t
+    #?=(yield (redis-async-wait-for-publish! sub))
+    (let loop ()
+      (let ((task (yield (redis-async-lpop pub "tasks"))))
+        (let-values (((id size frame)
+                      (match (read-from-string task)
+                        (`(,id ,size ,frame)
+                         (values id size frame)))))
+          (let1 image
+              (with-output-to-string
+                (lambda ()
+                  ((global-variable-ref raytracer-mod 'output-in-ppm-format)
+                   frame
+                   ((global-variable-ref raytracer-mod 'render-body) spheres size frame))))
+            (yield (redis-async-rpush pub "result" id))
+            (yield (redis-async-rpush pub "result" image))
+            (yield (redis-async-publish pub "done" task))))
+      ))
 
-  ;;   (while waiting (yield values))
-
-  ;;   (print (yield (redis-async-set pub "xxx" 123)))
-  ;;   (print "yyyyyyy")
-  ;;   (print (yield (redis-async-get pub "xxx")))
-  ;;   )
-
+    )
   (sys-exit 0)
   )
-
-#;(define update!
-  (let ((proc (gen-update-proc)))
-    (lambda (pub sub)
-      (print proc)
-      (set! proc (proc pub sub)))))
 
 (define (main args)
   (let ((pub (connect-redis))
@@ -86,10 +80,7 @@
     ((gen-update-proc) pub sub)
 
     (while #t
-      ;; (until (queue-empty? (ref pub 'hndl-queue)) (redis-async-update! pub))
-      ;; (until (queue-empty? (ref sub 'hndl-queue)) (redis-async-update! sub))
       (redis-async-update! pub)
       (redis-async-update! sub)
-      ;(update! pub sub)
       (sys-nanosleep (expt 10 8)))      ; sleep 100ms
   ))
